b19304dd50eac413cddd09006df97d45c75f7f49
tnyngyhng <tonynguyen003@web.de>
2022-01-25 22:51:11 +0100

    added name to code json and more curskill state

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -7,21 +7,32 @@ import './GameBoard.css';
 
 import game_list from '../assets/data/game_list.json';
 import character_table from '../assets/data/character_table.json';
+import skill_table from '../assets/data/skill_table.json';
+import opname_to_code from '../assets/data/opname_to_code.json';
 
 
 const GameBoard = () => {
   const { sub } = useParams();
-  
   const game = game_list.find(el => el['sub'] === sub)
+
   const char_names = Object.keys(character_table).map(key => character_table[key]['name']);
-  const skill_images = importAll(require.context('../assets/images/skills', false, /\.(png|jpe?g|svg)$/));
-  const skill_names = Object.keys(skill_images);
+  const char_names_lc = char_names.map(name => name.toLowerCase());
+  const char_img_paths = importAll(require.context('../assets/images/characters', false, /\.(png|jpe?g|svg)$/));
+  const skill_img_paths = importAll(require.context('../assets/images/skills', false, /\.(png|jpe?g|svg)$/));
+  const skill_img_names = Object.keys(skill_img_paths);
+  const skill_code_names = Object.keys(skill_table);
   
   const [userInput, setUserInput] = useState('');
   const [randNum, setRandNum] = useState();
+  const [curSkillCode, setCurSkillCode] = useState();
+  const [curScore, setCurScore] = useState(0);
 
   useEffect(() => {
-    setRandNum(parseInt(skill_names.length * Math.random()));
+    const random_number = parseInt(skill_img_names.length * Math.random());
+    setRandNum(random_number);
+    let [,,...codeStr] = skill_img_names[random_number].split('_');
+    codeStr = codeStr.join('').split('.')[0];
+    setCurSkillCode(codeStr);
   }, []);
   
   function importAll(r) {
@@ -31,12 +42,34 @@ const GameBoard = () => {
     }, {})
   }
 
+  const checkWinCon = () => {
+    if (!char_names_lc.includes(userInput.toLowerCase())) {
+      return false;
+    }
+    const user_char = char_names[char_names_lc.indexOf(userInput.toLowerCase())];
+    return character_table[opname_to_code[user_char]]['skills'].includes(curSkillCode);
+  };
+
+  const onClick = e => {
+    if (userInput === '') {
+      return;
+    }
+    const win_con = checkWinCon();
+    if (win_con) {
+
+    }
+    else {
+
+    }
+  }
+
   return (
     <div className='gameboard-container'>
       <h1>{game.title}</h1>
       <h2>{game.descr}</h2>
-      <img src={skill_images[skill_names[randNum]]} alt='todo'/>;
+      <img src={skill_img_paths[skill_img_names[randNum]]} alt='todo'/>
       <Suggestionbar suggestions={char_names} userInput={userInput} setUserInput={setUserInput} />
+      <button onClick={onClick}>Submit</button>
     </div>
   );
 };
