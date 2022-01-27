15865df4b558b35f4a8c29a8e8dbdba390272387
tnyngyhng <tonynguyen003@web.de>
2022-01-27 16:54:16 +0100

    added scoring to quiz

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Grow from '@mui/material/Grow';
 
 import game_list from '../assets/data/game_list.json';
 import character_table from '../assets/data/character_table.json';
@@ -29,17 +30,41 @@ const GameBoard = () => {
   // ['skchr_absin_1', 'skchr_aglina_1', ...]
   const skill_code_names = Object.keys(skill_table);
 
-  const [userInput, setUserInput] = useState('');
-  const [randNum, setRandNum] = useState();
+  const [userInput, setUserInput] = useState(null);
+  const [randNum, setRandNum] = useState(0);
   const [curSkillCode, setCurSkillCode] = useState('skchr_absin_1');
+  const [curSkillName, setCurSkillName] = useState('');
   const [curScore, setCurScore] = useState(0);
-  const [showResult, setShowResult] = useState(false);
+  const [showSolution, setShowSolution] = useState(false);
+  const [roundResult, setRoundResult] = useState(-1);
 
-  useEffect(() => {
+  const set_rand_skill = () => {
     const random_number = parseInt(skill_code_names.length * Math.random());
     setRandNum(random_number);
     setCurSkillCode(skill_code_names[random_number]);
+    console.log(skill_table[skill_code_names[random_number]]['op_names'][0]);
+  }
+
+  useEffect(() => {
+    set_rand_skill()
   }, []);
+
+  const load_new_round = () => {
+    setShowSolution(false);
+    setUserInput(null);
+    setCurScore(curScore + 1);
+    set_rand_skill();
+    setRoundResult(-1);
+  }
+
+  useEffect(() => {
+    let timer = null;
+    if (roundResult === 1) {
+      console.log('Timeout triggered');
+      timer = setTimeout(load_new_round, 3000);
+    }
+    return () => clearTimeout(timer);
+  }, [roundResult])
 
   function importAll(r) {
     return r.keys().reduce((prev, cur) => {
@@ -56,21 +81,34 @@ const GameBoard = () => {
     return character_table[opname_to_code[user_char]]['skills'].includes(curSkillCode);
   };
 
+  // if lose: show skill name, replace form with end screen + buttons
   const processRound = () => {
+    setCurSkillName(skill_table[curSkillCode]['name']);
     const win_con = checkWinCon();
-    console.log(win_con);
     if (win_con) {
-      setCurScore(curScore + 1);
-      setShowResult(true);
+      setShowSolution(true);
+      setRoundResult(1);
+    }
+    else {
+      setRoundResult(0);
     }
   }
 
   const onSubmit = e => {
-    console.log(userInput);
     if (!userInput) {
       return;
     }
     processRound();
+  }
+
+  const onSubmitEnter = e => {
+    if (e.keyCode === 13) {
+      if (!userInput) {
+        return;
+      }
+      console.log(userInput);
+      processRound();
+    }
   }
 
   return (
@@ -88,20 +126,41 @@ const GameBoard = () => {
           alt='skill icon to guess'
           mb={2}
         />
-        <Typography component='div' sx={{ mb: 2, display: showResult ? 'block' : 'none' }}>
-          {skill_table[curSkillCode]['name']}
-        </Typography>
-        <Box>
+        <Grow
+          unmountOnExit
+          in={showSolution}
+          sx={{ transformOrigin: '0 0 0', mb: 2 }}
+          {...(showSolution ? { timeout: 1000 } : {})}
+        >
+          <Typography component='div'>
+            {curSkillName}
+          </Typography>
+        </Grow>
+        <Box sx={{ display: roundResult ? 'block' : 'none' }}>
           <Autocomplete
-            inputValue={userInput}
-            onInputChange={(e, new_val) => setUserInput(new_val)}
-            freeSolo
+            value={userInput}
+            onChange={(e, new_val) => setUserInput(new_val)}
             disablePortal
+            autoHighlight
+            autoSelect
             options={char_names}
             sx={{ width: 300 }}
+            onKeyDown={onSubmitEnter}
             renderInput={tfProps => <TextField {...tfProps} label='Operator' />}
           />
           <Button variant='outlined' onClick={onSubmit}>Submit</Button>
+          <Typography component='div' variant='h6'>
+            Current Score: {curScore}
+          </Typography>
+        </Box>
+        <Box sx={{ display: roundResult ? 'none' : 'block' }}>
+          <Typography component='div'>
+            Gameover =(
+          </Typography>
+          <Typography component='div'>
+            Your final score: {curScore}
+          </Typography>
+          <Button>Play Again</Button>
         </Box>
       </Box>
     </>
