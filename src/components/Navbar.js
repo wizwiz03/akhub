ce8089ee96a1d498d7fb02bcea3ad551dd747f04
tnyngyhng <tonynguyen003@web.de>
2022-01-30 18:42:07 +0100

    navbar changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,96 +11,49 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Grid from '@mui/material/Grid';
+
+import HomeIcon from '@mui/icons-material/Home';
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
 
 const Navbar = () => {
-  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [value, setValue] = useState(0);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+  const handleChange = (e, newVal) => {
+    setValue(newVal);
   };
 
   const pages = { 'Home': '/', 'Games': '/games' };
   return (
     <AppBar position='static'>
       <Container>
-        <Toolbar >
-          <Typography
-            variant='h5'
-            noWrap
-            component='div'
-            sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
-          >
-            LOGO
-          </Typography>
-          <Typography
-            variant="h5"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'center' }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 2, display: {xs: 'none', md: 'flex'}, justifyContent: 'center' }}>
-            {
-              Object.keys(pages).map(page => (
-                <Button
-                  key={page}
-                  component={Link}
-                  to={pages[page]}
-                  sx={{ fontSize: 'body1.fontSize', display: 'block', color: 'white', mx: 1 }}
-                >
-                  {page}
-                </Button>
-              ))
-            }
-          </Box>
-          <Typography
-            noWrap
-            sx={{ flexGrow: 1, display: {xs: 'none', md: 'flex'}, justifyContent: 'end' }}
-            component='div'
-          >
-            Logout
-          </Typography>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="open menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {Object.keys(pages).map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography component={Link} to={pages[page]} textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+        <Toolbar>
+          <Grid container alignItems='center'>
+            <Grid item xs={1}>
+              <Typography
+                variant="h5"
+                component="div"
+              >
+                LOGO
+              </Typography>
+            </Grid>
+            <Grid item xs={10} container justifyContent='center'>
+              {
+                Object.keys(pages).map(page => (
+                  <Button
+                    key={page}
+                    sx={{ mx: {xs: 0, sm: 1}, color: 'white', fontSize: {xs: '1rem'} }}
+                    component={Link}
+                    to={pages[page]}
+                  >
+                    {page}
+                  </Button>
+                ))
+              }
+            </Grid>
+          </Grid>
         </Toolbar>
       </Container>
     </AppBar>
