96ac2050508a02a62f2f3f6a53c3a77a0ba594f2
wizwiz03 <tonynguyen003@web.de>
2022-02-06 23:49:31 +0100

    added random imgs to gameover

--- src/components/Gameover.js
+++ src/components/Gameover.js
@@ -5,14 +5,27 @@ import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import ReplayIcon from '@mui/icons-material/Replay';
 
-import gameover_img from '../dump/256_lost_1.png';
-
 const Gameover = ({ score_results, score, playAgain }) => {
+  const importAll = (r) => {
+    return r.keys().reduce((prev, cur) => {
+      prev[cur.replace('./', '').replace('.png', '')] = r(cur);
+      return prev;
+    }, {})
+  }
+
+  const stickers = {
+    bad: importAll(require.context('./assets/images/gameover/bad', false, /\.(png|jpe?g|svg)$/)),
+    decent: importAll(require.context('./assets/images/gameover/decent', false, /\.(png|jpe?g|svg)$/)),
+    great: importAll(require.context('./assets/images/gameover/great', false, /\.(png|jpe?g|svg)$/))
+  };
+
   const gameover_text = {
     bad: 'Looks like you have lost. You can\'t stop like this... You can do better! How about another try?',
     decent: 'Not bad. You seem to know a thing or two. Or you are just lucky... The only way to find out is to play more!',
     great: 'Wow, that\'s a nice score. Good Job! Here have a happy sticker as your reward.'
   };
+
+  const result = score <= score_results['bad'] ? 'bad' : score <= score_results['decent'] ? 'decent' : 'great';
 
   return (
     <Paper elevation={8} sx={{ padding: '24px 8px', flex: 1 }}>
@@ -20,12 +33,14 @@ const Gameover = ({ score_results, score, playAgain }) => {
         <Typography variant='h2'>Your final score:</Typography>
         <Typography variant='h1'>{score}</Typography>
         <Typography variant='p' textAlign='center'>
-          {score <= score_results['bad'] ? gameover_text['bad'] : score <= score_results['decent'] ? gameover_text['decent'] : gameover_text['great']}
+          {gameover_text[result]}
         </Typography>
-        <Button variant='outlined' endIcon={<ReplayIcon />} onClick={playAgain} sx={{ borderRadius: '8px' }}>
+        <Button variant='contained' endIcon={<ReplayIcon />} onClick={playAgain} sx={{ borderRadius: '8px' }}>
           Play Again
         </Button>
-        <Box component='img' src={gameover_img} alt='lost game image' sx={{ width: '256px' }} />
+        <Box component='img' alt='lost game image' sx={{ width: '256px' }}
+          src={Object.values(stickers[result])[parseInt(Object.keys(stickers[result]).length*Math.random())]}
+        />
       </Stack>
     </Paper>
   );
