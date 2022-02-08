679cd2559b11471f0f0fc929373328dad930553f
wizwiz03 <tonynguyen003@web.de>
2022-02-08 17:35:48 +0100

    fixed blemi profile descr and added reddit link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import RedditIcon from '@mui/icons-material/Reddit';
 
 const Footer = () => {
   const theme = useTheme();
@@ -12,20 +13,31 @@ const Footer = () => {
       backgroundColor: theme.palette.background.default,
       boxShadow: theme.shadows[4],
       backgroundImage: 'linear-gradient(rgba(255, 255, 255, 0.09), rgba(255, 255, 255, 0.09))'
-      }}>
+    }}>
       <Stack justifyContent='space-around' alignItems='center'>
         <Typography>Made by wizwiz03</Typography>
         <Stack>
           <Typography>For Bugs and Suggestions: </Typography>
-          <Button
-            href='https://github.com/wizwiz03/akhub'
-            target='_blank'
-            rel='noopener'
-            aria-label='github repo link'
-            size='large'
-          >
-            <GitHubIcon />
-          </Button>
+          <Stack direction='row' spacing={4} justifyContent='center'>
+            <Button
+              href='https://github.com/wizwiz03/akhub'
+              target='_blank'
+              rel='noopener'
+              aria-label='github repo link'
+              size='large'
+            >
+              <GitHubIcon />
+            </Button>
+            <Button
+              href='https://reddit.com/r/arknights/comments/snj06l/i_present_you_arknights_hub_a_website_with_fun/'
+              target='_blank'
+              rel='noopener'
+              aria-label='reddit thread link'
+              size='large'
+            >
+              <RedditIcon />
+            </Button>
+          </Stack>
         </Stack>
       </Stack>
     </Box>
